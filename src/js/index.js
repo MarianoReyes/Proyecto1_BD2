0188f4af3bb24530aad426dc47ca6888f348ab2a
MarianoReyes <josemarianoreyesh02@gmail.com>
2023-01-26 17:42:40 -0600

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,8 @@ const uri = "mongodb://localhost:27017/";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
     if (err) throw err;
-    const db = client.db("mydb");
-    const collection = db.collection("customers");
+    const db = client.db("inventario_empresa");
+    const collection = db.collection("productos");
     collection.insertOne({ name: "John", address: "Highway 37" }, function(err, res) {
         if (err) throw err;
         console.log("Document inserted");
